88165723cfeda64c3816e20377bc33e11d0f38cc
ziyuan <hanziyuan08@outlook.com>
2016-09-22 22:31:24 +0800

    Add testing.

--- welove/sig_test.go
+++ welove/sig_test.go
@@ -2,6 +2,9 @@ package welove
 
 import (
 	"testing"
+	"crypto/hmac"
+	"crypto/sha1"
+	"reflect"
 )
 
 func TestSig_Encode(t *testing.T) {
@@ -13,5 +16,15 @@ func TestSig_Encode(t *testing.T) {
 	sig := sigEncode.Encode(method, u, content)
 	if sig != "64DO4FLAHIkJADMTuv019C8vAas=" {
 		t.Error("Sig_Encode Error.")
+	}
+}
+
+func TestNewSig(t *testing.T) {
+	key := []byte("8b5b6eca8a9d1d1f")
+	mac := hmac.New(sha1.New, key)
+	s1 := NewSig(key)
+	s2 := &Sig{key, mac}
+	if reflect.DeepEqual(s1, s2) {
+		t.Error("NewSig Error.")
 	}
 }
