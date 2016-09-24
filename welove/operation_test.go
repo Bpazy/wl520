8662460884d938ab0a5caf4b8c4b9f3ad7e81d3c
ziyuan <hanziyuan08@outlook.com>
2016-09-24 10:36:17 +0800

    重构代码结构

--- welove/operation_test.go
+++ welove/operation_test.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"github.com/bitly/go-simplejson"
+	"encoding/json"
+	"fmt"
 )
 
 func TestTreePost(t *testing.T) {
@@ -15,7 +17,52 @@ func TestTreePost(t *testing.T) {
 	bytes, _ := ioutil.ReadAll(res.Body)
 	js, _ := simplejson.NewJson(bytes)
 	result, _ := js.Get("result").Int()
-	if result != 1 && result != 1001 {
+	if result != 1 && result != 1001 && result != 1002 {
+		t.Error("响应值result错误.")
+	}
+}
+
+func TestHomePost(t *testing.T) {
+	res, err := HomePost("562949961343086-2ca7e299a09974dd0", 7, "844424932415867")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	j := make(map[string]interface{})
+	json.Unmarshal(body, &j)
+	result := j["result"]
+	if result != 1201.0 && result != 1.0 {
+		log.Fatal("响应值result错误.")
+	}
+}
+
+func TestRandomHouse(t *testing.T) {
+	_, ok := RandomHouse("562949961343086-2ca7e299a09974dd0")
+	if !ok {
+		t.Error()
+	}
+}
+
+func TestVisit(t *testing.T) {
+	id, ok := RandomHouse("562949961343086-2ca7e299a09974dd0")
+	if !ok {
+		t.Error()
+	}
+	res, err := Visit("562949961343086-2ca7e299a09974dd0", id)
+	if err != nil {
+		t.Error(err)
+	}
+	bytes, _ := ioutil.ReadAll(res.Body)
+	js, err := simplejson.NewJson(bytes)
+	if err != nil {
+		t.Error(err)
+	}
+	result, _ := js.Get("result").Int()
+	if result != 1 && result != 1201 {
 		t.Error("响应值result错误.")
 	}
 }
--- welove/util.go
+++ welove/util.go
@@ -1,12 +1,40 @@
 package welove
 
 import (
+	"io"
+	"os"
 	"hash"
 	"crypto/hmac"
 	"crypto/sha1"
 	"net/url"
 	"encoding/base64"
 )
+
+type CustomOutput struct {
+	out []io.Writer
+}
+
+func New(out ...io.Writer) CustomOutput {
+	return CustomOutput{out}
+}
+
+func (c *CustomOutput) Add(out io.Writer) {
+	c.out = append(c.out, out)
+}
+
+func (c *CustomOutput) Write(p []byte) (int, error) {
+	var n int = 0
+	var err error = nil
+	for _, v := range c.out {
+		n, err = v.Write(p)
+	}
+	return n, err
+}
+
+func DefaultLog(path string) CustomOutput {
+	var file, _ = os.OpenFile(path, os.O_APPEND | os.O_CREATE, os.ModeAppend)
+	return New(os.Stdout, file)
+}
 
 type Sig struct {
 	key   []byte
