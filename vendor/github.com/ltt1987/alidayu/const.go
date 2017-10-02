24700169f2fe2636793ccead196ec44f51e704e0
ziyuan <hanziyuan08@outlook.com>
2017-10-02 12:56:15 +0800

    add godep

---
+++ vendor/github.com/ltt1987/alidayu/const.go
@@ -0,0 +1,13 @@
+package alidayu
+
+const (
+	URL_HTTPS         string = "https://eco.taobao.com/router/rest"
+	URL_HTTP          string = "http://gw.api.taobao.com/router/rest"
+	Method_SendSMS    string = "alibaba.aliqin.fc.sms.num.send"
+	Method_CallTTS    string = "alibaba.aliqin.fc.tts.num.singlecall"
+	Method_CallVoice  string = "alibaba.aliqin.fc.voice.num.singlecall"
+	Method_CallDouble string = "alibaba.aliqin.fc.voice.num.doublecall"
+)
+
+var AppKey string
+var AppSecret string
---
+++ vendor/github.com/ltt1987/alidayu/post.go
@@ -0,0 +1,44 @@
+package alidayu
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+var UseHTTP = false
+
+func DoPost(m map[string]string) (success bool, response string) {
+	if AppKey == "" || AppSecret == "" {
+		return false, "AppKey or AppSecret is requierd!"
+	}
+
+	body, size := getRequestBody(m)
+	client := &http.Client{}
+	var req *http.Request
+	var err error
+	if !UseHTTP {
+		req, err = http.NewRequest("POST", URL_HTTPS, body)
+	} else {
+		req, err = http.NewRequest("POST", URL_HTTP, body)
+	}
+	if err != nil {
+		return false, err.Error()
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.ContentLength = size
+
+	resp, err := client.Do(req)
+	if err != nil {
+		response = err.Error()
+		return
+	}
+	defer resp.Body.Close()
+
+	data, _ := ioutil.ReadAll(resp.Body)
+	response = string(data)
+	if strings.Contains(response, "success") {
+		return true, response
+	}
+	return false, response
+}
